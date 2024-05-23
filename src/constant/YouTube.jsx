b54efebb80236fe702a65b6c387eda178aa430eb
sumitme1620 <sumitme1620@gmail.com>
2024-05-23 09:14:04 +0530

    first commit

--- src/constant/YouTube.jsx
+++ src/constant/YouTube.jsx
@@ -1,4 +1,5 @@
-export const API_KEY = "AIzaSyCg1dI8UkAnYFfzXwKMm86rFBpgU-i2XLc"; //    AIzaSyCXiBFmb5g84SlQEJ54amfIHT3Ee0y43fM,  AIzaSyDD5BpZSzVz_mh1w079o8sZ2mpvsa6_gt8
+export const API_KEY =
+  "AIzaSyCg1dI8UkAnYFfzXwKMm86rFBpgU-i2XLc AIzaSyDD5BpZSzVz_mh1w079o8sZ2mpvsa6_gt8"; //    AIzaSyCXiBFmb5g84SlQEJ54amfIHT3Ee0y43fM,  AIzaSyDD5BpZSzVz_mh1w079o8sZ2mpvsa6_gt8
 export const BASE_URL = "https://www.googleapis.com/youtube/v3";
 export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`;
 export const SEARCH_SUGGETION_API =
